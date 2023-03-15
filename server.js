58349a078afdece7c95fa4e458d59c93a0caa2c6
= <=>
2023-03-15 17:26:39 -0400

    Update view engine

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
 const express = require('express');
 const app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger)
+app.use('/public', express.static('public'));
+
 app.get('/', (req, res) => {
     console.log("yeap");
     res.render('index');
-    res.send("Hi");
 })
+
+const userRouter = require('./routes/users');
+const aboutRouter = require('./routes/about');
+const resumeRouter = require('./routes/resume');
+const contactRouter = require('./routes/contact');
+
+app.use('/about', aboutRouter)
+app.use('/users', userRouter)
+app.use('/resume', resumeRouter)
+app.use('/contact', contactRouter)
+
+function logger(req, res, next) {
+    console.log(req.originalUrl)
+    next()
+}
+
 
 app.listen(3000)
 
