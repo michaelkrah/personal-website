ff319d685af9717bf3b66a252438f04ddb1f7e43
1 <1>
2023-10-12 23:04:19 -0400

    updating website

--- server.js
+++ server.js
@@ -6,20 +6,25 @@ app.set('view engine', 'ejs');
 app.use(logger)
 app.use('/public', express.static('public'));
 
-app.get('/', (req, res) => {
-    console.log("yeap");
-    res.render('index');
-})
+
 
 const userRouter = require('./routes/users');
 const aboutRouter = require('./routes/about');
 const resumeRouter = require('./routes/resume');
 const contactRouter = require('./routes/contact');
+const songRouter = require('./routes/songs');
 
 app.use('/about', aboutRouter)
 app.use('/users', userRouter)
 app.use('/resume', resumeRouter)
 app.use('/contact', contactRouter)
+app.use('/songs', songRouter)
+
+
+app.get('/', (req, res) => {
+    console.log("yeap");
+    res.render('index');
+})
 
 function logger(req, res, next) {
     console.log(req.originalUrl)
